31f383a97c3889d276ada02110cb17f2271eb992
joa0-pedro <joaopedro.dev20@gmail.com>
2024-09-20 01:33:00 -0300

    project settings

---
+++ api/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FavoriteWordController;
+use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WordController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json(['message' => 'Fullstack Challenge 🏅 - Dictionary']);
+});
+
+Route::post('/auth/signUp', [AuthController::class, 'signUp']);
+Route::post('/auth/signIn', [AuthController::class, 'signIn']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+// Route::middleware(['auth'])->group(function () {
+Route::get('/entries/en', [WordController::class, 'index']);
+Route::get('/entries/en/{word}', [WordController::class, 'show']);
+
+Route::post('/entries/en/{word}/favorite', [FavoriteWordController::class, 'store']);
+Route::delete('/entries/en/{word}/unfavorite', [FavoriteWordController::class, 'destroy']);
+
+Route::get('/user/me', [UserController::class, 'show']);
+Route::get('/user/me/history', [HistoryController::class, 'index']);
+Route::get('/user/me/favorites', [FavoriteWordController::class, 'index']);
+// });
